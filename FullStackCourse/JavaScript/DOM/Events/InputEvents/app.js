709605e7dc6b34fc5ac8c739c9202d7ab209a190
AlptekinOcakdan <alptekin.ocakdan@gmail.com>
2021-11-11 00:01:56 +0300

    Js DOM Events

---
+++ FullStackCourse/JavaScript/DOM/Events/InputEvents/app.js
@@ -0,0 +1,33 @@
+const filter = document.getElementById("filter");
+
+
+// document.addEventListener("DOMContentLoaded",load);
+//
+// function load(e) {
+//     console.log("Sayfa Yüklendi");
+// }
+
+
+//focus
+// filter.addEventListener("focus",run);
+
+//blur
+// filter.addEventListener("blur",run);
+
+//Past
+// filter.addEventListener("paste",run);
+
+//Copy
+// filter.addEventListener("copy",run);
+
+//Cut
+// filter.addEventListener("cut",run);
+
+//Select
+// filter.addEventListener("select",run);
+
+
+function run(e) {
+    console.log(e.type);
+}
+
---
+++ FullStackCourse/JavaScript/DOM/Events/KeyboardEvents/app.js
@@ -0,0 +1,35 @@
+//Klavye Eventleri
+const header = document.querySelector(".card-header");
+const todoInput = document.querySelector("#todo");
+
+todoInput.addEventListener("keyup", changeText);
+
+function changeText(e) {
+    header.textContent = e.target.value;
+    // console.log(e.target.value);
+}
+
+//keypress
+// document.addEventListener("keypress",run);
+//
+// function run(e) {
+//     console.log(e.which);
+//     console.log(e.key);
+//     console.log("Naber");
+// }
+//keydown
+// document.addEventListener("keydown",run);
+//
+// function run(e) {
+//     console.log(e.which);
+//     console.log(e.key);
+//     console.log("Naber");
+// }
+//keyup
+// document.addEventListener("keyup",run);
+//
+// function run(e) {
+//     console.log(e.which);
+//     console.log(e.key);
+//     console.log("Naber");
+// }
