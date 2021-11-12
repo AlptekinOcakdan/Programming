c34adf98de1f5b556f9f025199e729187b140796
AlptekinOcakdan <alptekin.ocakdan@gmail.com>
2021-11-12 14:50:46 +0300

    Js ES6+ Features

---
+++ FullStackCourse/JavaScript/ES6+/ArrowFunction/app.js
@@ -0,0 +1,39 @@
+/*
+const merhaba=function () {
+    console.log("merhaba");
+
+
+}
+merhaba();
+*/
+
+//Arrow Function
+
+/*
+const merhaba=()=>{
+    console.log("merhaba")
+}
+
+merhaba();
+*/
+
+/*
+const merhaba = (firstName,lastName) => console.log("Merhaba", firstName,lastName);
+
+merhaba("Murat","Coşkun");
+*/
+
+/*
+const cube=function (x) {
+    return x*x*x;
+}
+console.log(cube(4);
+*/
+
+//return işlemi olduğunda
+
+/*
+const cube = (x) => x*x*x*x;
+
+console.log(cube(4));
+*/
