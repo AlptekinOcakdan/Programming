476418b38673a79578a6e35ae5b6beea74079734
Alptekin <alptekin.ocakdan@gmail.com>
2022-06-20 13:04:45 +0300

    Learning the existence of Reactstrap...

--- FullStackCourse/React/intro/src/App.js
+++ FullStackCourse/React/intro/src/App.js
@@ -2,15 +2,29 @@ import React from 'react';
 import Navi from "./Navi";
 import CategoryList from "./CategoryList";
 import ProductList from "./ProductList";
+import {Col, Container, Row} from "reactstrap";
 
 function App() {
-  return (
-    <div>
-        <Navi/>
-        <CategoryList/>
-        <ProductList/>
-    </div>
-  );
+    return (
+        <div>
+            <Container>
+                <Row>
+                    <Navi/>
+                </Row>
+                <Row>
+                    <Col xs="3">
+                        <CategoryList/>
+                    </Col>
+                    <Col xs="9">
+                        <ProductList/>
+                    </Col>
+
+                </Row>
+            </Container>
+
+
+        </div>
+    );
 }
 
 export default App;
