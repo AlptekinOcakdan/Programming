b00e5d19ae32019fa537ef1b99b4180a65f73d5c
Alptekin <alptekin.ocakdan@gmail.com>
2022-06-20 13:55:01 +0300

    Using Props and learning how to Encapsulation with it.

--- FullStackCourse/React/intro/src/App.js
+++ FullStackCourse/React/intro/src/App.js
@@ -5,6 +5,8 @@ import ProductList from "./ProductList";
 import {Col, Container, Row} from "reactstrap";
 
 function App() {
+    let productInfo = {title:"Product List"};
+    let categoryInfo = {title:"Category List"};
     return (
         <div>
             <Container>
@@ -13,10 +15,10 @@ function App() {
                 </Row>
                 <Row>
                     <Col xs="3">
-                        <CategoryList/>
+                        <CategoryList info={categoryInfo}/>
                     </Col>
                     <Col xs="9">
-                        <ProductList/>
+                        <ProductList info={productInfo}/>
                     </Col>
 
                 </Row>
--- FullStackCourse/React/intro/src/CategoryList.js
+++ FullStackCourse/React/intro/src/CategoryList.js
@@ -5,7 +5,7 @@ class CategoryList extends Component {
     render() {
         return (
             <div>
-                <h3>Category List</h3>
+                <h3>{this.props.info.title}</h3>
                 <ListGroup>
                     <ListGroupItem>
                         Cras justo odio
