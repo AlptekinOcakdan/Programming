e6f88b56fc7977690bdb991065eacc9c1a289b64
Alptekin <alptekin.ocakdan@gmail.com>
2022-08-05 14:32:06 +0300

    Redux Thunk

--- FullStackCourse/React/northwind-redux/src/components/categories/CategoryList.js
+++ FullStackCourse/React/northwind-redux/src/components/categories/CategoryList.js
@@ -1,13 +1,44 @@
 import React, {Component} from 'react';
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+import * as categoryActions from "../../redux/actions/categoryActions"
+import {ListGroup, ListGroupItem} from "reactstrap";
 
 class CategoryList extends Component {
+    componentDidMount() {
+        this.props.actions.getCategories()
+    }
+
     render() {
         return (
             <div>
-                <h3>Category List</h3>
+                <h3>Categories</h3>
+                <ListGroup>
+                    {
+                        this.props.categories.map(category => (
+                            <ListGroupItem key={category.id}>
+                                {category.categoryName}
+                            </ListGroupItem>
+                        ))}
+                </ListGroup>
             </div>
-        );
+        )
     }
 }
 
-export default CategoryList;
+function mapStateToProps(state) {
+    return {
+        currentCategory: state.changeCategoryReducer,
+        categories: state.categoryListReducer
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: {
+            getCategories: bindActionCreators(categoryActions.getCategories, dispatch)
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CategoryList);
