5ce74be51b00286ac77facf4dc4e22edba4d9d42
Alptekin <alptekin.ocakdan@gmail.com>
2022-08-11 16:29:24 +0300

    Angular Intro, Demo project

---
+++ FullStackCourse/Angular7/intro/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  exports: [
+    ProductComponent
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- FullStackCourse/Angular7/shop/src/app/product/product.component.ts
+++ FullStackCourse/Angular7/shop/src/app/product/product.component.ts
@@ -1,19 +1,13 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product',
-  template: `
-    <p>
-      {{name}}
-    </p>`,
+  templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
 
-  constructor() {
-  }
-
-  name = "Laptop";
+  constructor() { }
 
   ngOnInit(): void {
   }
