f83b42c6e67e25c1756e899bedf394b2e880b790
Alptekin <alptekin.ocakdan1@ogr.sakarya.edu.tr>
2022-09-20 10:19:52 +0300

    Complete code of First Project.

--- arin-react/first-project/src/components/Card.js
+++ arin-react/first-project/src/components/Card.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-const Card =(props)=>{
-  return(
-      <div>
-          <div className="card" style={{width:"18rem"}}>
-              <img src="..." className="card-img-top" alt="..."/>
-              <div className="card-body">
-                  <h5 className="card-title">{props.cardTitle}</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk
-                      of the card's content.</p>
-                  <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-              </div>
-          </div>
-      </div>
-  );
-};
+class Card extends React.Component {
+
+    static defaultProps = {
+        cardTitle: "Default Card Title"
+    }
+
+    render() {
+        return (
+            <div className="card w-100">
+                <img src={this.props.image} className="card-img-top" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">{this.props.cardTitle}</h5>
+                    <p className="card-text">{this.props.cardText}</p>
+                    <p className="card-text"><small className="text-muted">{this.props.updatedTime}</small></p>
+                </div>
+            </div>
+        );
+    }
+}
+
+Card.propTypes = {
+    cardText: PropTypes.string.isRequired
+}
 
 export default Card;
+
+
