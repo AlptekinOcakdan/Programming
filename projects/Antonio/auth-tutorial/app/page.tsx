5dd02accd40017ffe827da749529a16e2a751ccd
Alptekin <alptekin.ocakdan@gmail.com>
2024-03-23 23:23:37 +0300

    auth tutorial initial commit

---
+++ projects/Antonio/auth-tutorial/app/page.tsx
@@ -0,0 +1,10 @@
+import {Button} from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <>
+            <p className="font-semibold text-green-500">Hello Auth</p>
+            <Button>Click Me!</Button>
+        </>
+    );
+}
