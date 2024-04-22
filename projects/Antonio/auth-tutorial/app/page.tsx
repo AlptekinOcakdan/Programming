b3932e3e5e9f06d84c0c04d73a51664b040f340e
Alptekin <alptekin.ocakdan@gmail.com>
2024-04-22 14:28:44 +0300

    Home Page

--- projects/Antonio/auth-tutorial/app/page.tsx
+++ projects/Antonio/auth-tutorial/app/page.tsx
@@ -1,10 +1,32 @@
+import {Poppins} from "next/font/google";
+import {cn} from "@/lib/utils";
 import {Button} from "@/components/ui/button";
+import {LoginButton} from "@/components/auth/login-button";
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"]
+});
 
 export default function Home() {
     return (
-        <>
-            <p className="font-semibold text-green-500">Hello Auth</p>
-            <Button>Click Me!</Button>
-        </>
+        <main
+            className="flex h-full flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+            <div className="space-y-6">
+                <h1 className={cn("text-6xl font-semibold text-white drop-shadow-md",font.className)}>
+                    Auth Tutorial
+                </h1>
+                <p className="text-white text-lg">
+                    This is a tutorial on how to implement authentication in your Next.js app.
+                </p>
+                <div>
+                    <LoginButton>
+                        <Button variant="secondary" size="lg">
+                            Sign In
+                        </Button>
+                    </LoginButton>
+                </div>
+            </div>
+        </main>
     );
 }
